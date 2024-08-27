f781fc4a20cb4a296d236b5f8b1808814cf6d5b4
Chandra <ryan.chandra.hadi@gmail.com>
2024-08-27 12:55:58 +0700

    feat: init base project structure

---
+++ pos-management-app/src/app/pages/invoice/invoice-edit/invoice-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-invoice-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './invoice-edit.component.html',
+  styleUrl: './invoice-edit.component.scss'
+})
+export class InvoiceEditComponent {
+
+}
--- pos-management-app/src/app/pages/product/product-edit/product-edit.component.ts
+++ pos-management-app/src/app/pages/product/product-edit/product-edit.component.ts
@@ -24,6 +24,7 @@ export class ProductEditComponent {
     this.productService.update(this.product.id, this.product).subscribe(updatedProduct => {
       this.productUpdated.emit(updatedProduct);
       alert("Product updated!");
+      this.router.navigate(['/product']);
     });
   }
 
